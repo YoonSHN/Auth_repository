9e06ab84171cbcb10d3790ff3234da267d28e15d
윤승현 <tkdenddl182@gmail.com>
2025-06-23 03:21:29 +0900

    feature : googleEmail Auth #1 Success

---
+++ src/main/java/com/example/insta_project/EmailService.java
@@ -0,0 +1,5 @@
+package com.example.insta_project;
+
+public interface EmailService {
+    void sendEmail(String to, String subject, String text);
+}
---
+++ src/test/java/com/example/insta_project/InstaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insta_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
